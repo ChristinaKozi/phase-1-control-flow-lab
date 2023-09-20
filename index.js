5cf4a0c6857e8ccbc09adbb872cc7698f66704a0
Christina Kozanian <christinakozanian@gmail.com>
2023-09-20 16:31:03 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let sample
+  if (ride <= 400) {
+    sample = "This one is on me!";
+  }
+  else if (ride > 400 && ride <= 2000) {
+    sample = "That will be twenty bucks.";
+  }
+  else if (ride > 2000 && ride <= 2500) {
+    sample = "I will gladly take your thirty bucks.";
+  }
+  else if (ride > 2500) {
+    sample = "No can do.";
+  }
+  return sample;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === "NYC" ? "Ok, sounds good." : "No go." ;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = "Thank you so much."
+      break;
+    case 'not as generous':
+      message = "Thank you."
+      break;
+    default:
+      message = "Bye."
+      break;
+  }
+  return message
 }
